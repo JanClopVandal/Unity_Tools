017522534c87c2575633665f9003611c490ee18c
danil matvienko <81968795+JanClopVandal@users.noreply.github.com>
2025-04-26 20:03:17 +0300

    RuntimeSavedComponents
    Saves changes to specific components via the context menu in Play Mode.

---
+++ Assets/Editor/RuntimeSavedComponents/RuntimeComponentSaverEditor.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Component), true)]
+public class RuntimeComponentSaverEditor : Editor
+{
+    private void OnEnable()
+    {
+        hideFlags = HideFlags.NotEditable;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+
+    [MenuItem("CONTEXT/Component/💾 Save Runtime Changes")]
+    private static void SaveRuntimeChanges(MenuCommand command)
+    {
+        Debug.Log("Saving runtime changes...");
+        if (!Application.isPlaying)
+        {
+            Debug.LogWarning("This only works in Play Mode.");
+            return;
+        }
+
+        var component = command.context as Component;
+        if (component == null)
+        {
+            Debug.LogError("Context is not a Component.");
+            return;
+        }
+
+        RuntimeSavedComponentsManager.SaveComponent(component);
+    }
+}
+#endif
